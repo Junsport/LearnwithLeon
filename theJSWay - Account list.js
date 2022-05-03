35cc15642121862722580b8c054ea4f85746d6b1
sergio flores <junie33@gmail.com>
2022-05-03 14:50:40 -0700

    theJSWay - Account list.js

--- theJSWay - Account list.js
+++ theJSWay - Account list.js
@@ -1,15 +1,29 @@
+// Account list
+
+// Let's build upon a previous account object exercise. A bank account is still defined by:
+
+//A name property.
+//A balance property, initially set to 0.
+//A credit method adding the value passed as an argument to the account balance.
+//A describe method returning the account description.
+
+// Write a program that creates three accounts: one belonging to Sean, another to Brad and the third one to Georges. These accounts are stored in an array. Next, the program credits 1000 to each account and shows its description.
+
 class Account {
     constructor(name, balance){
         this.name = name
-        this.balance = balance 
+        this.balance = 0 
     }
-    credit(){
-        this.balance += 1000
+    credit(amount){
+        this.balance += amount
     }
     describe(){
-        console.log( `owner: ${this.name}, balance: ${this.balance}` )
+        return `owner: ${this.name}, balance: ${this.balance}` 
     }
 }
-// let sean = new Account (sean,0)
-// let Brad = new Account (Brad,0)
-// let Georges = new Account (Georges,0)
+const accountList = [new Account ('sean'), new Account ('Brad'), new Account ('Georges')]
+
+accountList.forEach(item => {
+    item.credit(+(1000))
+    console.log(item.describe())
+})
